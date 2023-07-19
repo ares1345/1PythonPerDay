dd8d658b817250f541deacfdc31bc2447067b4d6
ares1345 <ares1345istaken@gmail.com>
2023-07-19 12:58:19 +0200

    Premier Programmes

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('/home/joe/repos/1PythonPerDay/Programs')
+#NOTE: Pour que le programme fonctionne, il faut changer ce PATH avec celui ou est installés le programmes
+
+#-------------------------------------------------------------------------------------------------------------
+
+
+
+#-------------------------------------------------------------------------------------------------------------
+
+
