48534df501cef502471973ebccb856e28a63ec82
ares1345 <ares1345istaken@gmail.com>
2023-07-20 22:35:52 +0200

    Avancement programme

--- Programs/imc.py
+++ Programs/imc.py
@@ -1,5 +1,16 @@
 def imc():
     print()
+    res = 0.0
     poids = int(input("Quelle est votre poids? (en kilo) "))
     unitTaille = input("Quelle est l'unité de la taille que vous comptez utiliser? (M ou CM) ")
-    
+    if unitTaille == "M":
+        taille = float(input("Veuillez indiquer votre taille (en M) "))
+        res = poids / (taille * taille)
+    elif unitTaille == "CM":
+        taille = int(input("Quelle est votre taille (en CM) "))
+        taille/= 100
+        res = poids / (taille * taille)
+    print()
+    print(f"Votre IMC est de {res}")
+
+
