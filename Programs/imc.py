75ba2ed199b26ba5ffbc9e2bbc9b534e8a0b5aef
ares1345 <ares1345istaken@gmail.com>
2023-07-20 22:29:50 +0200

    Ajout de .gitignore

---
+++ Programs/imc.py
@@ -0,0 +1,5 @@
+def imc():
+    print()
+    poids = int(input("Quelle est votre poids? (en kilo) "))
+    unitTaille = input("Quelle est l'unité de la taille que vous comptez utiliser? (M ou CM) ")
+    
