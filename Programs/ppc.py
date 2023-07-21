5a19dbf5c96479ee6eaa112cb8b816e8605408dc
ares1345 <ares1345istaken@gmail.com>
2023-07-21 15:46:35 +0200

    Avancement sur le jeu

--- Programs/ppc.py
+++ Programs/ppc.py
@@ -7,8 +7,20 @@ def ppc():
     nbPartie = int(input("Combien de partie souhaitez-vous jouer? "))
     while nbPartie <= 0:
         nbPartie = int(input("Erreur, le nombre de partie que vous avez entrés est égal à 0 ou est un nombre négatif, veuillez réessayez"))
+    
     for i in range(nbPartie):
         choixUser = input("Que choisissez-vous, pierre, papier ou ciseau?")
         choixProg = range.randint(1, 3)
         if (choixUser.upper() == "PIERRE" and choixProg == 3) or (choixUser.upper() == "PAPIER" and choixProg == 1) or (choixUser.upper() == "CISEAU" and choixProg == 2):
+            print()
             print(f"Vous avez gagné! ")
+            print()
+            scoreUser += 1
+
+        if (choixUser.upper() == "PIERRE" and choixProg == 2) or (choixUser.upper() == "PAPIER" and choixProg == 3) or (choixUser.upper() == "CISEAU" and choixProg == 1):
+            print()
+            print(f"Vous avez perdu! ")
+            print()
+            scoreProg += 1
+
+    if 
