eaccd022b7673a72c50468b0e225e373a0f6c228
ares1345 <ares1345istaken@gmail.com>
2023-07-22 22:36:52 +0200

    Programme terminé

--- Programs/calcDepense.py
+++ Programs/calcDepense.py
@@ -2,6 +2,14 @@ def calcBudget():
     print()
     depense = 0
     total = 0
-    while depense == -0:
-        depense = float(input("Quelle est la dépense? (entrer -0 si vous avez terminé.) "))
+    while depense != -1:
+        depense = float(input("Quelle est la dépense? (entrer -1 si vous avez terminé.) "))
         total += depense
+    print()
+    print(f"Le total de vos dépenses est de {total}. ")
+    print("""
+    
+    Le programme est terminé, Vous allez être redirigé vers le programme principal
+           -------------------------------------------------------------------------
+         """)
+    
