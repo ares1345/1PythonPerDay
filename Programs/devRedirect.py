ada3068445b9b96fb51691394fa7f86db70d25c5
ares1345 <ares1345istaken@gmail.com>
2023-07-23 23:00:06 +0200

    Programme terminé

--- Programs/devRedirect.py
+++ Programs/devRedirect.py
@@ -41,10 +41,11 @@ def devRedirect():
             Une ressource qui peut vous apprendre n'importe quel langage de programmation sur une application. À noter que certaines fonctionnalités sont dérrières un paywall.    
         
         100 days of python par Dr. Angela Yu (Udemy) [udemy.com/course/100-days-of-code/] (2):
-            Connu pour son bootcamp sur le développement web, Angela Yu a aussi un cours sur Python mais qui est instruit différament. Au lieu d'écouter des cours, vous codez chaque jour un projet différent et qui vous permet de vous instruire 
+            Connu pour son bootcamp sur le développement web, Angela Yu a aussi un cours sur Python mais qui est instruit différament. Au lieu d'écouter des cours, vous codez chaque jour un projet différent et qui vous permet de vous instruire sur python avec de la pratique. À noter que ce cours est payant.
+
             
         education.github.com (3):
-            Pack d'offres partenaires de Github, il vous faut simplement prouver que vous êtes étudiants, peu importe votre niveau de scolarité. Je vous recommende si vous préferer les outils de Jetbrain ou souhaitez apprendre le développement web en général avec des sites plus établis (Frontend Masters, Educative...) et d'autres outils pour vous aidez au mieux.
+            Pack d'offres partenaires de Github, il vous faut simplement prouver que vous êtes étudiants, peu importe votre niveau de scolarité. Je vous recommende de l'utiliser si vous préferer les outils de Jetbrain ou souhaitez apprendre le développement web en général avec des sites plus établis (Frontend Masters, Educative...) et d'autres outils pour vous aidez au mieux.
         
         """))
 
@@ -52,11 +53,17 @@ def devRedirect():
             choixLien = int(input("Erreur. Veuillez entrer un choix valide. "))
 
         if choixLien == 1:
-            webbrowser.open("https://freecodecamp.org")
+            webbrowser.open("https://sololearn.com")
         elif choixLien == 2:
-            webbrowser.open("https://theodinproject.com")
+            webbrowser.open("https://www.udemy.com/course/100-days-of-code/")
         elif choixLien == 3:
             webbrowser.open("https://education.github.com")
+
+    print("""
+    
+    Le programme est terminé, Vous allez être redirigé vers le programme principal
+           -------------------------------------------------------------------------
+         """)
     
 
 devRedirect()
