c0b42aee806cd89bec6d6bf428bb9c41e0538412
ares1345 <ares1345istaken@gmail.com>
2023-07-23 22:46:01 +0200

    Avancement

--- Programs/devRedirect.py
+++ Programs/devRedirect.py
@@ -6,12 +6,13 @@ def devRedirect():
     print("Ceci est un hub de liens intéressent pour la programmation. ")
     print()
     choix = int(input("Quel sujet vous intérsse-t-il aujourd'hui? (1 pour développement web, 2 pour python) "))
-    while choix != 1 or choix != 2:
+    while (choix != 1) and (choix != 2):
         print()
-        choix = int(input("Erreur. Veuillez rentrez un choix valide. "))
+        choix = input("Erreur. Veuillez rentrez un choix valide. ")
     
     if choix == 1:
         choixLien = int(input("""Vous pouvez choisir le nombre associé au site pour être redirigé au site avec votre navigateur
+        
         freecodecamp.org (1):
             Une très bonne ressource pour commencer le développement web, mais qui vous prend par la mains, il se pourrait donc que vous n'aimiez par ce que vous êtes en train de créer.
         
@@ -22,3 +23,5 @@ def devRedirect():
             Pack d'offres partenaires de Github, il vous faut simplement prouver que vous êtes étudiants, peu importe votre niveau de scolarité. Je vous recommende si vous préferer les outils de Jetbrain ou souhaitez apprendre le développement web en général avec des sites plus établis (Frontend Masters, Educative...) et d'autres outils pour vous aidez au mieux.
         
         """))
+
+devRedirect()
