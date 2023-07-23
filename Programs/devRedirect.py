dfd6fb1b141440b6049d043036d42230dd196c2a
ares1345 <ares1345istaken@gmail.com>
2023-07-23 22:25:13 +0200

    Avancement cinquième programme

--- Programs/devRedirect.py
+++ Programs/devRedirect.py
@@ -1,3 +1,8 @@
 import webbrowser
+#webbrowser.open("https://google.com")
 
-webbrowser.open("https://google.com")
+def devRedirect():
+    print()
+    print("Ceci est un hub de liens intéressent pour la programmation. ")
+    print()
+    choix = int(input("Quel sujet vous intérsse-t-il aujourd'hui? (1 pour développement web, 2 pour python) "))
