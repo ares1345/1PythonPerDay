5d33254d21c58042a54a4033a8f7f52e29468192
ares1345 <ares1345istaken@gmail.com>
2023-07-24 23:00:33 +0200

    Ajout avertissement

---
+++ Programs/downloaderYT.py
@@ -0,0 +1,8 @@
+from pytube import YouTube as YT
+from pytube.cli import on_progress #this module contains the built in progress bar. 
+
+def downloaderYT():
+    print()
+    print("Ce programme a juste une valeur illustrative pour montrer mes capacités en programmation. Je condamne avec la plus grande fermeté le téléchargement de vidéo sans passer par une manière approuvé par Youtube. ")
+    print()
+    print()
