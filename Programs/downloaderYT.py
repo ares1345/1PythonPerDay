18c3b5afa3198500c46196ba9df15e8923f55a95
ares1345 <ares1345istaken@gmail.com>
2023-07-24 23:10:26 +0200

    Avancement

--- Programs/downloaderYT.py
+++ Programs/downloaderYT.py
@@ -3,6 +3,10 @@ from pytube.cli import on_progress #this module contains the built in progress b
 
 def downloaderYT():
     print()
-    print("Ce programme a juste une valeur illustrative pour montrer mes capacités en programmation. Je condamne avec la plus grande fermeté le téléchargement de vidéo sans passer par une manière approuvé par Youtube. ")
+    print("Ce programme a juste une valeur illustrative pour montrer mes capacités en programmation. Je condamne avec la plus grande fermeté le téléchargement de vidéos sans passer par une manière approuvé par Youtube. ")
     print()
     print()
+    lien = input("Veuillez entrer le lien de la vidéo qui sera télécharger dans le dossier 'videos'. ")
+    
+
+downloaderYT()
