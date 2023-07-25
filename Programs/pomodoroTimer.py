dd8f3a32a5b9f645846e7209ecb53c49c8392c93
ares1345 <ares1345istaken@gmail.com>
2023-07-25 22:14:43 +0200

    Avancement

--- Programs/pomodoroTimer.py
+++ Programs/pomodoroTimer.py
@@ -9,6 +9,12 @@ def pomodoroTimer():
     choix = input("Souhaitez-vous utilisez les paramètres par défaut? (O pour oui, N pour non) ")
 
     if choix.upper() == "O":
-        for i in range(1500, 0, -1):
-            secondes = i % 60
+        for i in range(4):
+            for j in range(1500, 0, -1):
+                secondes = j % 60
+                minutes = int(j/60) % 60
+                heures = int(x/3600)
+                print(f"{heures:02}:{minutes:02}:{secondes:02}", end="\r")
+                time.sleep(1)
+            
             
