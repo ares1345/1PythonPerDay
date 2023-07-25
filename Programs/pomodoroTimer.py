7b322f3f337905009a716d066b4e5781299e6787
ares1345 <ares1345istaken@gmail.com>
2023-07-25 22:23:57 +0200

    Avancement

--- Programs/pomodoroTimer.py
+++ Programs/pomodoroTimer.py
@@ -1,10 +1,7 @@
 import time
 
 def pomodoroTimer():
-    defaut = 1500
-    defautRep = 4
-    defautRepot = 5
-    longDefautRepot = 15
+    print()
 
     choix = input("Souhaitez-vous utilisez les paramètres par défaut? (O pour oui, N pour non) ")
 
@@ -17,6 +14,7 @@ def pomodoroTimer():
                 print(f"{heures:02}:{minutes:02}:{secondes:02}", end="\r")
                 time.sleep(1)
             
+            print()
             print("PÉRIODE DE TRAVAIL TERMINÉ, 5 MINUTES DE REPOS COMMENCE")
             
             for k in range(300, 0, -1):
@@ -25,11 +23,16 @@ def pomodoroTimer():
                     heures = int(k/3600)
                     print(f"{heures:02}:{minutes:02}:{secondes:02}", end="\r")
                     time.sleep(1)
+            
+            print()
+            print("PÉRIODE DE REPOS TERMINÉ, PÉRIODE DE 25 MINUTES DE TRAVAIL COMMENCE ")
         
-        for l in range(300, 0, -1):
+        for l in range(900, 0, -1):
             secondes = l % 60
             minutes = int(l/60) % 60
             heures = int(l/3600)
             print(f"{heures:02}:{minutes:02}:{secondes:02}", end="\r")
             time.sleep(1)
+
+        
             
