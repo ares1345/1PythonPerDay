91286751e27f4ee6e10a1f48c888811f94d83567
ares1345 <ares1345istaken@gmail.com>
2023-07-25 22:30:09 +0200

    Avancement

--- Programs/pomodoroTimer.py
+++ Programs/pomodoroTimer.py
@@ -27,12 +27,18 @@ def pomodoroTimer():
             print()
             print("PÉRIODE DE REPOS TERMINÉ, PÉRIODE DE 25 MINUTES DE TRAVAIL COMMENCE ")
         
-        for l in range(900, 0, -1):
-            secondes = l % 60
-            minutes = int(l/60) % 60
-            heures = int(l/3600)
-            print(f"{heures:02}:{minutes:02}:{secondes:02}", end="\r")
-            time.sleep(1)
+
+
+
+    if choix.upper() == "N":
+        tempsTr = int(input("Combien de minutes pour votre période de travail? "))
+        tempsRep = int(input("Combien de minutes pour votre période de repos? "))
+        nbRep = int(input("Combien de fois souhaitez-vous répéter le temps de travail? "))
+
+        tempsTr *= 60
+        tempsRep *= 60
+        for i in range(nbRep):
+             
 
         
             
